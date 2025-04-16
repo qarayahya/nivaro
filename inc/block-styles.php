4e7bf0d27ecde5c1e73e0119156b5e1f9594e62d
Qara Yahya <qara.yahya@gmail.com>
2025-04-16 12:44:59 +0100

    Fixed: Corrected early translation loading for the nivaro text domain

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -5,26 +5,38 @@
  *
  * @package nivaro
  * @author Yahya Qara
- * @since 1.0.0
+ * @since 1.0.5
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Register block styles for core/button.
-register_block_style(
-	'core/button',
-	array(
-		'name'  => 'secondary-button',
-		'label' => __( 'Secondary', 'nivaro' ),
-	)
-);
-
-register_block_style(
-	'core/button',
-	array(
-		'name'  => 'tertiary-button',
-		'label' => __( 'Tertiary', 'nivaro' ),
-	)
-);
+// Registers custom block styles.
+if ( ! function_exists( 'nvro_block_styles' ) ) :
+	/**
+	 * Registers custom block styles.
+	 *
+	 * @since 1.0.5
+	 *
+	 * @return void
+	 */
+	function nvro_block_styles() {
+		register_block_style(
+			'core/button',
+			array(
+				'name'  => 'secondary-button',
+				'label' => __( 'Secondary', 'nivaro' ),
+			)
+		);
+		
+		register_block_style(
+			'core/button',
+			array(
+				'name'  => 'tertiary-button',
+				'label' => __( 'Tertiary', 'nivaro' ),
+			)
+		);
+	}
+endif;
+add_action( 'init', 'nvro_block_styles' );
